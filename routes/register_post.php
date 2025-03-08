30336137a40c320f18bd0b220b93276c579c1883
EmperorFiw <thewaphithak47@gmail.com>
2025-03-08 22:07:43 +0700

    ปรับระบความปลอดภัย, ความสะดวก login register

--- routes/register_post.php
+++ routes/register_post.php
@@ -6,13 +6,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'] ?? '';
     $email = $_POST['email'] ?? '';
     $phone = $_POST['phone'] ?? '';
+    $token = $_POST['token'] ?? '';
 
-    if (!empty($username) && !empty($password) && !empty($email) && !empty($phone)) {
+    if (empty($token) || !isset($_SESSION['token']) || $_SESSION['token'] !== $token) {
+        swalAlert('Token mismatch', 'error', 'register_get', 'register');
+    }
+    else if (!empty($username) && !empty($password) && !empty($email) && !empty($phone)) {
         $users = new Users();
         $result = $users->register($username, $password, $email, $phone);
 
         if ($result === "ลงทะเบียนสำเร็จ!") {
             swalAlert($result, 'success', 'register_get', 'home');
+            
+            if (isset($_SESSION['token']))
+            {
+                unset($_SESSION['token']);
+            }
         } else {
             swalAlert($result, 'error', 'register_get', 'register');
         }
